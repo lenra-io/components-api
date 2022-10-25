64684741e03e96f0d371aa5c6c9f4af4a4fb81ee
shiipou <shiishii@nocturlab.fr>
2022-10-25 14:17:23 +0200

    fix: Fix components-api in each defs URL Fixes #125
    Signed-off-by: shiipou <shiishii@nocturlab.fr>

--- docs/src/components/propertyTable.jsx
+++ docs/src/components/propertyTable.jsx
@@ -58,7 +58,7 @@ import ${name} from '../../../${api_path}';
 <PropertyTable schema={${name}}/>
 `)
         }
-        res = <a href={"/components-api/" + property['$ref'].replace("json", "html")}>{property['$ref']}</a>;
+        res = <a href={"/" + property['$ref'].replace("json", "html")}>{property['$ref']}</a>;
     }
     return res;
 }
